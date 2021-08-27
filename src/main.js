6e0095082fe9f64002d6078d166351a9d4d62a2b
Emmanuel <Emmanuelsumeh@gmail.com>
2021-08-27 12:13:08 -0700

    added smart contract functionalities, fixed create book error, added fields , implemented ipfs update

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import './style.css'
 import { create } from 'ipfs-http-client'
 import { newKitFromWeb3 } from '@celo/contractkit'
 import Web3 from "@celo/contractkit/node_modules/web3"
+import BigNumber from "bignumber.js";
 import booklibraryAbi from '../contract/booklibrary.abi.json'
 
-const BooklibraryContractAddress = "0x0D5690Bd51A71EFcED3c61F69e792a8C7C046f59";
+// erc20 decimals
+const ERC20_DECIMALS = 18;
+
+// previous address
+// const BooklibraryContractAddress = "0x0D5690Bd51A71EFcED3c61F69e792a8C7C046f59";
+const BooklibraryContractAddress = "0x91d505e8F9A7ccF4Ca5B2147cfB42d9BC6aD84b7"
+
 const ipfsClient = create({
   url: 'https://ipfs.infura.io:5001/api/v0'
 })
@@ -55,7 +62,11 @@ const getBooks = async function() {
         image: p[2],
         isbn: p[3],
         date: p[4],
-        summary: p[5]
+        summary: p[5],
+        // ipfs hash of the book
+        book: p[6],
+        // cost of the book
+        cost:   new BigNumber(p[7]),
       })
     })
     _books.push(_book)
@@ -64,6 +75,8 @@ const getBooks = async function() {
   renderBooks()
 }
 
+
+// upload file to IPFS
 const uploadHelper = async (_file) => {
   try {
     const file = await ipfsClient.add(_file);
@@ -80,13 +93,20 @@ document
   .querySelector("#submit-book")
   .addEventListener("click", async (e) => {
     const selectedImage = document.getElementById("select-image").files[0];
+    const selectedBook = document.getElementById("select-book").files[0];
     const ipfs_bookImage = await uploadHelper(selectedImage);
+    const ipfs_book_doc = await uploadHelper(selectedBook);
+    const cost = document.getElementById("input-cost").value
+    console.log({cost})
     const bookParams = [
       document.getElementById("input-title").value,
       ipfs_bookImage,
       document.getElementById("input-isbn").value,
       document.getElementById("input-date").value,
       document.getElementById("input-summary").value,
+      // the actual book from ipfs
+      ipfs_book_doc,
+      BigNumber(cost).shiftedBy(ERC20_DECIMALS),
     ]
 
     bookNotification(`⌛ Adding "${bookParams[0]}"...`)
@@ -101,6 +121,27 @@ document
       bookNotification(`⚠️ ${error}.`)
     }
   })
+
+
+  // rent a book
+  document
+  .querySelector("#rentBook")
+  .addEventListener("click", async (e) => {
+  
+
+    bookNotification(`⌛ Renting "${bookParams[0]}"...`)
+    try {
+      await contract.methods.addBook(...bookParams).send({
+        from: kit.defaultAccount
+      })
+        bookNotification(`🎉 You successfully added "${bookParams[0]}".`)
+        getBooks()
+      
+    } catch (error) {
+      bookNotification(`⚠️ ${error}.`)
+    }
+  })
+
 
 function renderBooks() {
   document.getElementById("AvailableBooks").innerHTML = ""
@@ -129,7 +170,11 @@ function bookTemplate(_book) {
         <p class="card-text mb-4" style="min-height: 82px">
           ${_book.date}             
         </p>
-          </div>
+
+        <p class="card-text mb-4" style="min-height: 82px">
+        ${BigNumber((_book.cost).shiftedBy(-ERC20_DECIMALS).toString()) } cUSD            
+      </p>
+      </div>
     </div>
   `
 }
